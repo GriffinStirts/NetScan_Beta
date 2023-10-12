1cb12d04fa703a58b63020e66f26ac9dee0f5069
GriffinStirts <griffinstirts@gmail.com>
2023-10-12 13:44:17 -0600

    All Files From Solution added

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace NetScanBeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.manualSSIDCheckBox = new System.Windows.Forms.CheckBox();
+            this.ssidTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.ResultsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(282, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan for Networks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // manualSSIDCheckBox
+            // 
+            this.manualSSIDCheckBox.AutoSize = true;
+            this.manualSSIDCheckBox.Location = new System.Drawing.Point(329, 203);
+            this.manualSSIDCheckBox.Name = "manualSSIDCheckBox";
+            this.manualSSIDCheckBox.Size = new System.Drawing.Size(124, 17);
+            this.manualSSIDCheckBox.TabIndex = 2;
+            this.manualSSIDCheckBox.Text = "Enter SSID Manually";
+            this.manualSSIDCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ssidTextBox
+            // 
+            this.ssidTextBox.Location = new System.Drawing.Point(320, 226);
+            this.ssidTextBox.Name = "ssidTextBox";
+            this.ssidTextBox.Size = new System.Drawing.Size(145, 20);
+            this.ssidTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(320, 270);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(145, 20);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SSID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(351, 335);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 7;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);            
+            // ResultsLabel
+            // 
+            this.ResultsLabel.AutoSize = true;
+            this.ResultsLabel.Location = new System.Drawing.Point(209, 375);
+            this.ResultsLabel.Name = "ResultsLabel";
+            this.ResultsLabel.Size = new System.Drawing.Size(42, 13);
+            this.ResultsLabel.TabIndex = 8;
+            this.ResultsLabel.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultsLabel);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.ssidTextBox);
+            this.Controls.Add(this.manualSSIDCheckBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox manualSSIDCheckBox;
+        private System.Windows.Forms.TextBox ssidTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label ResultsLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using NativeWifi;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NetScanBeta
+{
+    public partial class Form1 : Form
+    {
+        private NetworkInterface connectedNetworkInterface; // Store the connected network interface
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Clear the ListBox before scanning and populating
+            listBox1.Items.Clear();
+
+            // Scan and populate available WiFi networks
+            ScanAndPopulateWiFiNetworks();
+        }
+
+        private void ScanAndPopulateWiFiNetworks()
+        {
+            try
+            {
+                WlanClient client = new WlanClient();
+                foreach (WlanClient.WlanInterface wlanInterface in client.Interfaces)
+                {
+                    Wlan.WlanAvailableNetwork[] networks = wlanInterface.GetAvailableNetworkList(0);
+
+                    foreach (Wlan.WlanAvailableNetwork network in networks)
+                    {
+                        // Add the SSID to the ListBox
+                        string ssid = Encoding.ASCII.GetString(network.dot11Ssid.SSID, 0, (int)network.dot11Ssid.SSIDLength);
+                        listBox1.Items.Add(ssid);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check if manual SSID entry is enabled
+                if (manualSSIDCheckBox.Checked)
+                {
+                    // Use the manually entered SSID and password
+                    string manualSSID = ssidTextBox.Text;
+                    string password = passwordTextBox.Text;
+
+                    // Implement the connection logic using manualSSID and password
+                    connectedNetworkInterface = ConnectToWiFiNetwork(manualSSID, password);
+                }
+                else
+                {
+                    // Get the selected SSID from the ListBox
+                    string selectedSSID = listBox1.SelectedItem as string;
+
+                    if (string.IsNullOrEmpty(selectedSSID))
+                    {
+                        MessageBox.Show("Please select an SSID from the list.", "Error");
+                    }
+                    else
+                    {
+                        // Implement the connection logic using selectedSSID and passwordTextBox.Text
+                        connectedNetworkInterface = ConnectToWiFiNetwork(selectedSSID, passwordTextBox.Text);
+                    }
+                }
+
+                // After connecting, retrieve and display the MAC address manufacturer
+                if (connectedNetworkInterface != null)
+                {
+                    string macAddress = connectedNetworkInterface.GetPhysicalAddress().ToString();
+                    string manufacturer = await GetMacAddressManufacturerAsync(macAddress);
+                    ResultsLabel.Text = $"Connected to network. MAC Address: {macAddress}, Manufacturer: {manufacturer}";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+
+        public static NetworkInterface ConnectToWiFiNetwork(string ssid, string password)
+        {
+            try
+            {
+                // Create the folder for WLAN profiles if it doesn't exist
+                string profilesFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\WLANProfiles";
+                if (!Directory.Exists(profilesFolder))
+                {
+                    Directory.CreateDirectory(profilesFolder);
+                }
+
+                // Use the netsh command to configure and connect to the WiFi network
+                string profileName = $"WiFiProfile_{Guid.NewGuid()}";
+                string xml = $@"<?xml version='1.0'?>
+        <WLANProfile xmlns='http://www.microsoft.com/networking/WLAN/profile/v1'>
+            <name>{profileName}</name>
+            <SSIDConfig>
+                <SSID>
+                    <name>{ssid}</name>
+                </SSID>
+            </SSIDConfig>
+            <connectionType>ESS</connectionType>
+            <connectionMode>manual</connectionMode>
+            <MSM>
+                <security>
+                    <authEncryption>
+                        <authentication>WPA2PSK</authentication>
+                        <encryption>AES</encryption>
+                        <useOneX>false</useOneX>
+                    </authEncryption>
+                    <sharedKey>
+                        <keyType>passPhrase</keyType>
+                        <protected>false</protected>
+                        <keyMaterial>{password}</keyMaterial>
+                    </sharedKey>
+                </security>
+            </MSM>
+        </WLANProfile>";
+
+                // Save the profile
+                string profilePath = Path.Combine(profilesFolder, $"{profileName}.xml");
+                File.WriteAllText(profilePath, xml);
+
+                // Add the profile
+                var psi = new ProcessStartInfo("netsh", $"add profile filename=\"{profilePath}\"");
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(psi).WaitForExit();
+
+                // Connect to the network
+                psi = new ProcessStartInfo("netsh", $"wlan connect name=\"{ssid}\"");
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(psi).WaitForExit();
+
+                // Remove the temporary profile
+                psi = new ProcessStartInfo("netsh", $"delete profile name=\"{profileName}\"");
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(psi).WaitForExit();
+
+                // Get the connected network interface
+                NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+                foreach (NetworkInterface networkInterface in networkInterfaces)
+                {
+                    if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                    {
+                        return networkInterface;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+
+            return null;
+        }
+
+        public async Task<string> GetMacAddressManufacturerAsync(string macAddress)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Send a request to a MAC address manufacturer lookup service
+                    string apiUrl = $"https://macvendors.co/api/{macAddress}";
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        if (content.Contains("result not found"))
+                        {
+                            return "Manufacturer not found";
+                        }
+
+                        // Parse the JSON response to get the manufacturer
+                        dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+                        return data["result"]["company"];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error looking up MAC address manufacturer: " + ex.Message, "Error");
+            }
+
+            return "Unknown";
+        }
+    }
+}
